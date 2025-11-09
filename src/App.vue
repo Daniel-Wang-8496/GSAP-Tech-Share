f6bbf39e9c68b2e24759d31877a58113a29ac05a
nguy4803 <nguy4803@umn.edu>
2025-11-09 04:05:55 -0600

    Added basic scroll card animation

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,102 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import { gsap } from "gsap";
-import SplashGsap from './components/SplashGsap.vue'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { onMounted } from "vue";
+
+gsap.registerPlugin(ScrollTrigger);
+
+// Initialize animations after DOM is loaded
+onMounted(() => {
+  gsap.utils.toArray('.reveal-text').forEach(elem => {
+      gsap.from(elem, {
+          duration: 1,
+          opacity: 0,
+          y: 40,
+          ease: "power2.out",
+          scrollTrigger: {
+              trigger: elem,
+              start: "top 85%",
+              toggleActions: "play none none none"
+          }
+      });
+  });
+
+
+  gsap.from(".about-card", {
+      duration: 0.8,
+      opacity: 0,
+      y: 50,
+      stagger: 0.2,
+      ease: "power2.out",
+      scrollTrigger: {
+          trigger: "#about",
+          start: "top 70%",
+          toggleActions: "play none none none"
+      }
+  });
+});
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
   <main>
-    <SplashGsap/>
+    <div style="height: 100vh; display: flex; align-items: center; justify-content: center;">
+      Scroll down to see the animation
+    </div>
+    
+  <!-- idk what cards could be. some features, values, etc. -->
+  <div id="about">
+      <div class="container">
+          <h2 class="reveal-text">How we work</h2>
+          <div class="about-grid pure-g">
+              <div class="pure-u-1 pure-u-md-1-3">
+                <div class="about-card">
+                    <h3>1. Card</h3>
+                    <p>Test1</p>
+                </div>
+              </div>
+
+              <div class="pure-u-1 pure-u-md-1-3">
+                <div class="about-card">
+                    <h3>2. Card</h3>
+                    <p>Test2</p>
+                </div>
+              </div>
+              
+              <div class="pure-u-1 pure-u-md-1-3">
+                <div class="about-card">
+                    <h3>3. Card</h3>
+                    <p>Test3</p>
+                </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+
+#about {
+  color: white;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.about-card {
+  background: black;
+  border-radius: 0.75rem;
+  padding: 2rem;
+  margin: 1rem;
+  text-align: center;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.reveal-text {
+  text-align: center;
+  font-size: 2rem;
+  margin-bottom: 3rem;
 }
+
 </style>
