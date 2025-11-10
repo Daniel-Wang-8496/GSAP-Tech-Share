59a015bed8bb6ac3304e53097e3a75cb7f6088b2
Gunnar J. <GunnarJag@gmail.com>
2025-11-09 22:51:09 -0600

    basics of animation

--- src/components/EasingGsap.vue
+++ src/components/EasingGsap.vue
@@ -8,7 +8,7 @@ onMounted(() => {
   gsap.set(".box", { x: 0 });
 });
 
-function animateBoxBase() {
+function animateBoxNoStagger() {
   if(state.value==false){
     gsap.killTweensOf('.box');
     gsap.to('.box', { x: 500, backgroundColor: 'red'});
@@ -20,23 +20,106 @@ function animateBoxBase() {
   state.value = !state.value;
 }
 
-function animateBoxSnap() {
+function animateBoxBase() {
   if(state.value==false){
-    gsap.set('.box', { x: 500, backgroundColor: 'red'});
+    gsap.killTweensOf('.box');
+    gsap.to('.box', { x: 500, backgroundColor: 'red', stagger: 0.2});
   }
   else if(state.value==true){
-    gsap.set('.box', { x: 0, backgroundColor: 'blue'});
+    gsap.killTweensOf('.box');
+    gsap.to('.box', { x: 0, backgroundColor: 'blue', stagger: 0.2});
   }
   state.value = !state.value;
 }
+
+function animateBoxSnap() {
+  if(state.value==false){
+    gsap.set('.box', { x: 500, backgroundColor: 'red', stagger: 0.2});
+  }
+  else if(state.value==true){
+    gsap.set('.box', { x: 0, backgroundColor: 'blue', stagger: 0.2});
+  }
+  state.value = !state.value;
+}
+
+function animateBoxLong() {
+  if(state.value==false){
+    gsap.to('.box', { x: 500, backgroundColor: 'red', duration: 3, stagger: 0.2});
+  }
+  else if(state.value==true){
+    gsap.to('.box', { x: 0, backgroundColor: 'blue', duration: 3, stagger: 0.2});
+  }
+  state.value = !state.value;
+}
+
+function animateBoxRoll() {
+  if(state.value==false){
+    gsap.to('.box', { x: 500, backgroundColor: 'red', rotation:90, stagger: 0.2});
+  }
+  else if(state.value==true){
+    gsap.to('.box', { x: 0, backgroundColor: 'blue', rotation:0, stagger: 0.2});
+  }
+  state.value = !state.value;
+}
+
+function easingBounce() {
+  if(state.value==false){
+    gsap.killTweensOf('.box');
+    gsap.to('.box', { x: 500, backgroundColor: 'red', ease: "bounce.out", duration: 1.5, stagger: 0.2});
+  }
+  else if(state.value==true){
+    gsap.killTweensOf('.box');
+    gsap.to('.box', { x: 0, backgroundColor: 'blue', ease: "bounce.out", duration: 1.5, stagger: 0.2});
+  }
+  state.value = !state.value;
+}
+
+function easingPower() {
+  if(state.value==false){
+    gsap.killTweensOf('.box');
+    gsap.to('.box', { x: 500, backgroundColor: 'red', ease: "power3.out", duration: 1.5, stagger: 0.2});
+  }
+  else if(state.value==true){
+    gsap.killTweensOf('.box');
+    gsap.to('.box', { x: 0, backgroundColor: 'blue', ease: "power3.out", duration: 1.5, stagger: 0.2});
+  }
+  state.value = !state.value;
+}
+
+function easingSteps() {
+  if(state.value==false){
+    gsap.killTweensOf('.box');
+    gsap.to('.box', { x: 500, backgroundColor: 'red', ease: "steps(5)", duration: 1.5, stagger: 0.2});
+  }
+  else if(state.value==true){
+    gsap.killTweensOf('.box');
+    gsap.to('.box', { x: 0, backgroundColor: 'blue', ease: "steps(5)", duration: 1.5, stagger: 0.2});
+  }
+  state.value = !state.value;
+}
+
 </script>
 
 <template>
   <div class="box"></div>
   <br/>
+  <div class="box"></div>
+  <br/>
+  <div class="box"></div>
+  <br/>
   <div>
     <button @click="animateBoxBase">Base Move</button>
     <button @click="animateBoxSnap">Snap Move</button>
+    <button @click="animateBoxLong">Long Move</button>
+    <button @click="animateBoxRoll">Roll Move</button>
+  </div>
+  <div>
+    <button @click="easingBounce">Bounce Ease</button>
+    <button @click="easingPower">Power Ease</button>
+    <button @click="easingSteps">Steps Ease</button>
+  </div>
+  <div>
+    <button @click="animateBoxNoStagger">No Stagger</button>
   </div>
 </template>
 
